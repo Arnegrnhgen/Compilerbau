181e6013ce45758d92bd2172d0fb60ce4f9c501e
Christian <christian@desktopdebian>
2018-05-17 21:14:53 +0200

    cleanup

--- exercise2/TypeChecker.hs
+++ exercise2/TypeChecker.hs
@@ -6,61 +6,23 @@ import ErrM
 import qualified Data.Map as Map
 import Control.Monad
 
+
 type Env = (Sigs, [Context], Structs)
 type Sigs = Map.Map Id ([Type],Type)
 type Context = Map.Map Id Type
 type Structs = Map.Map Id Struct
 type Struct = Map.Map Id Type
 
-{-
-checkfunctypes ::  [String] -> [String] -> String
-checkfunctypes [] types = ""
-checkfunctypes (t:args) types = (if (elem t types ) then "" else "type not found: " ++ t ++ "\n") ++ checkfunctypes args types
-
-
-  --foldl (++) "" ["type not found: " ++ x ++ "\n" |TypeId (Id x) <- [ x | x<- t:[t2 | ( ADecl t2 _ ) <- args ],not (x `elem` types)] ]
-
-
-
-
-checkdef :: Env -> Def -> Err Env
-checkdef (DFun t (Id "main") args _) (Con types _) =  (if (not (t == Type_int)) then "main() must return int\n"  else "") ++
-                                                      (if (not (null args))     then "main() must not take any parameters\n" else "") ++
-                                                      checkfunctypes [t2 | (ADecl (TypeId (Id t2)) _) <- (ADecl t (Id "")):args] (Map.keys types)
-
-
-checkdef (DFun t _ args _) (Con types _)           =  checkfunctypes [t2 | (ADecl (TypeId (Id t2)) _) <- (ADecl t (Id "")):args] (Map.keys types)
-
-
---checkdef (DStruct _ fields) (Con types _) = checkfunctypes [t | (FDecl (TypeId (Id t)) _) <- fields] (Map.keys types)
-checkdef (DStruct _ fields) (Con types _) = ""
-
-regdef :: Def -> Context -> Context
-regdef (DFun t (Id i) args _) (Con types m) = Con types (Map.insert i (t,args) m)
-regdef (DStruct _ fields) context = context
---regdef (DStruct (Id i) fields ) (Con types m)= (Con (Map.insert i fields types) m)
-
-checkdefs :: [Def] -> Context -> (String,Context)
-checkdefs [] context = ("",context)
-checkdefs (d:defs) context  = (checkdef d context ++ msg , context2)
-                                where (msg,context2 ) = (checkdefs defs (regdef d context) )
-
-   -}
-
 
 {-
     TODO:
-	inferExpr (page 72)
-	finish checkstm
-	    - eval expr
-	    - extend context
-	finish checkDef for structs
-	add function parameters to new block context
-	check function returntype
-	inferUnaryExpr(++i,i++) - inferBinExpr(a+b)
-	use printTree
-	implement inferNumBin()
+        finish checkstm
+            - eval expr
+            - extend context
+        check function returntype
+        use printTree
 -}
+
 
 lookupCtx :: [Context] -> Id -> Err Type
 lookupCtx [] (Id str) = (Bad ("identifier " ++ str ++ " not found"))
@@ -72,13 +34,16 @@ lookupCtx (ctx:ctxs) ident = case (Map.lookup ident ctx) of
 lookupVarEnv :: Env -> Id -> Err Type
 lookupVarEnv (sigs,ctxs,_) ident = lookupCtx ctxs ident
 
+
 lookupSigs :: Sigs -> Id -> [Exp] -> Err ([Type], Type)
 lookupSigs sigs (Id str) args = case (Map.lookup (Id str) sigs) of
                                     Nothing -> (Bad ("function identifier " ++ str ++ " not found"))
                                     Just x -> Ok x
 
+
 lookupFunEnv :: Env -> Id -> [Exp] -> Err ([Type], Type)
 lookupFunEnv (sigs,ctxs,_) ident args = lookupSigs sigs ident args
+
 
 checkFunArgs :: Env -> [Exp] -> ([Type],Type) -> Err Type
 checkFunArgs env exps (args,rettype) = foldM compareArgs rettype (zip exps args) --TODO: check same length
@@ -87,20 +52,25 @@ checkFunArgs env exps (args,rettype) = foldM compareArgs rettype (zip exps args)
                                                     t2 <- inferExpr env expr
                                                     if t == t2 then Ok rtype else Bad ("function argument type mismatch: " ++ show (inferExpr env expr) ++ " vs " ++ show t)
 
+
 insertVarEnv :: Env -> Type -> Id -> Env
 insertVarEnv (sigs,[],structs) t i       = (sigs,([Map.fromList [(i,t)]]),structs)
 insertVarEnv (sigs,(c:ctxs),structs) t i = (sigs,(Map.insert i t c) : ctxs,structs)
+
 
 insertVarsEnv :: Env -> Type -> [Id] -> Env
 insertVarsEnv env t [] = env
 insertVarsEnv env t (i:is) = insertVarsEnv (insertVarEnv env t i) t is
 
+
 insertFunVar :: Env -> Arg -> Env
 insertFunVar (sigs,(c:ctxs),structs) (ADecl t i) = (sigs, (Map.insert i t c) : ctxs,structs)
+
 
 insertFunVars :: Env -> [Arg] -> Err Env
 insertFunVars env [] = Ok env
 insertFunVars env (a:args) = insertFunVars (insertFunVar env a) args
+
 
 lookUpStructField :: Env -> Id -> Type -> Err Type
 lookUpStructField (sigs,ctxs,structs) membername (TypeId structname) = case Map.lookup structname structs of
@@ -110,6 +80,7 @@ lookUpStructField (sigs,ctxs,structs) membername (TypeId structname) = case Map.
                                                                         Just field -> return field
 lookUpStructField (sigs,ctxs,structs) membername structname = Bad (show structname ++ " not a struct")
 
+
 checkExpType :: Env -> Type -> Exp -> Err Type
 checkExpType env typ expr = do
                             typ2 <- inferExpr env expr
@@ -117,6 +88,7 @@ checkExpType env typ expr = do
                               return typ
                             else
                               fail $ "type of " ++ printTree expr ++ " expected " ++ printTree typ ++ " but found " ++ printTree typ2
+
 
 inferBin :: [Type] -> Env -> Exp -> Exp -> Err Type
 inferBin types env exp1 exp2 = do
@@ -126,6 +98,7 @@ inferBin types env exp1 exp2 = do
                                          checkExpType env typ exp2
                                        else
                                          fail $ "wrong type of expression " ++ printTree exp1
+
 
 inferNumBin :: Env -> Exp -> Exp -> Err Type
 inferNumBin env exp1 exp2 = do
@@ -140,6 +113,7 @@ inferNumBin env exp1 exp2 = do
                                                  Type_double -> return Type_double
                                                  otherwise -> fail $ "wrong type of second numeric expression " ++ printTree exp2
                                 otherwise -> fail $ "wrong type of first numeric expression " ++ printTree exp1
+
 
 inferExpr :: Env -> Exp -> Err Type
 inferExpr env ETrue = Ok Type_bool
@@ -175,6 +149,7 @@ inferExpr env (EAss lhs rhs) = do
                                 rhs <- inferExpr env rhs
                                 if lhs == rhs then return lhs else Bad ("bad assignment from " ++ show rhs ++ " to " ++ show lhs)
 
+
 checkStm :: Env -> Stm -> Err Env
 checkStm env (SReturn e) = Ok env --TODO
 checkStm env (SExp e) = case (inferExpr env e) of
@@ -188,11 +163,13 @@ checkStm env (SWhile e s)
 checkStm env (SBlock ss) = Ok env --TODO
 checkStm env (SIfElse e s1 s2) = Ok env --TODO
 
+
 envNewBlock :: Env -> Err Env
-envNewBlock (sigs,c,structs) = Ok (sigs, [Map.fromList []] ++ c,structs)
+envNewBlock (sigs,c,structs) = return (sigs, [Map.fromList []] ++ c,structs)
+
 
 envPopBlock :: Env -> Err Env
-envPopBlock (sigs,c,structs) = Ok (sigs, tail c, structs)
+envPopBlock (sigs,c,structs) = return (sigs, tail c, structs)
 
 
 checkStms :: Env -> [Stm] -> Err Env
