a9dc9fc4db2fa301be233f366131f1690bb2767c
Christian <christian@dlaptop>
2018-06-16 23:18:16 +0200

    add test infrastructure

---
+++ exercise2.5/tests/driver.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+set -eu
+
+CMD=../lab3
+
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+cd "$SCRIPTPATH"
+
+echo "Starting tests..."
+
+for filename in ./test_*.cc; do
+	echo -n "Running test $filename..."
+	
+	clang "$filename" -o "$filename.clang.bin"
+	if "$filename.clang.bin"; then
+		ref_val=0
+	else
+		ref_val=$?
+	fi
+	echo -n "  $ref_val"
+
+	"$CMD" "$filename" > "$filename.custom.ll"
+	llc "$filename.custom.ll" -o "$filename.custom.s"
+	clang "$filename.custom.s" -o "$filename.custom.bin"
+	if "$filename.custom.bin"; then
+		act_val=0
+	else
+		act_val=$?
+	fi
+
+	if [ $act_val -ne $ref_val ]; then
+		echo "  failure: custom $act_val vs clang $ref_val"
+		exit 1;
+	fi
+
+	echo "  done"
+	rm "$filename.clang.bin" "$filename.custom.ll" "$filename.custom.bin" "$filename.custom.s"
+
+done
+
+echo "Finished tests"
---
+++ exercise2.5/tests/test_2.cc
@@ -0,0 +1,13 @@
+int main() {
+	int i1;
+	int i2, i3, i4;
+	int i5 = 2;
+	int i6 = i5;
+	i2 = i1 = 3;
+	i4 = i3 = i1;
+	i1 = i1 + 2;
+	i2 = 2 + i2;
+	i3 = 2 + i4 + 3 + i5;
+
+	return i1 + i2 + i3 + i4 + i5 + i6;
+}
